ad22e51c93c8d9079b7ec2d895b7be24e5d70a90
Noah Hampton <noahjunk@bradyhampton.com>
2021-04-07 13:16:45 -0400

    Fixed bugs in loops

--- CSIS111_Lab3B/CSIS111_Lab3B.cpp
+++ CSIS111_Lab3B/CSIS111_Lab3B.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 int main()
 {
-	vector <int> g1;
-	for (inti = 1; i <= 5, i++) {
+	vector<int> g1;
+	for (int i = 1; i <= 5; i++) {
 		g1.push_back(i);
 	}
 	cout << "Numbers in vector: ";
-	for (inti = 0; i < g1.size(); i++) {
+	for (int i = 0; i < g1.size(); i++) {
 		cout << g1[i] << " ";
 	}
 	cout << "\n\nNumbers in reverse: ";
